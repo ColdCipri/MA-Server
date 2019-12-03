7cc38f3961bb7fe0c0b4490e689e92e949994214
ColdCipri <colder.ciprian@gmail.com>
2019-12-03 13:09:04 +0200

    Add project files.

---
+++ Android-Server/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Android_Server.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Android-Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Android_Server.WebApiApplication" Language="C#" %>
